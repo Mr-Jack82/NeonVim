56ec5623cb70c5cbd6500fe3e2dcd2231bc77c85
Vitaly Shevchenko <blackjack358@gmail.com>
2021-08-25 14:27:03 +0300

    Bring in line with the source

--- lua/config.lua
+++ lua/config.lua
@@ -23,10 +23,6 @@ local M = {
     -- @value: number
     cmdheight = 2,
 
-    -- Minimal number of screen lines to keep above and below the cursor.
-    -- @value: number
-    scrolloff = 10,
-
     -- Number of spaces to use for each step of (auto)indent and number
     -- of spaces that a <Tab> counts for.
     -- @value: number
@@ -37,34 +33,9 @@ local M = {
     -- @value: boolean
     relativenumber = true,
 
-    -- Highlight the screen line of the cursor.
-    -- @value: boolean
-    cursorline = true,
-
-    -- When on, lines longer than the width of the window will wrap and
-    -- displaying continues on the next line.
-    -- @value: boolean
-    word_wrap = false,
-
-    -- Keep the last cursor position when reopening a buffer.
-    -- @value: boolean
-    preserve_cursor = true,
-
-    -- Hightlight text when yanked.
-    -- @value: boolean
-    highlight_yank = true,
-
-    -- Chars for trailing space, eol, tab, etc.
-    -- @value: boolean
-    listchars = false,
-
     -- Adds indentation guides to all lines .
     -- @value: boolean
     indent_guides = true,
-
-    -- Basic trim of trailing white space.
-    -- @value: boolean
-    trim_trailing_space = true,
 
     -- Format on save.
     -- @value: boolean
@@ -78,7 +49,7 @@ local M = {
     -----------------------------------------------------------------------------//
     -- Press <space>ht to see all available themes.
     -- @value: string
-    colorscheme = "onepro",
+    colorscheme = "neon",
 
     -- These option only apply for themes available with themes-nvim.
     -- See :h themes-nvim for help.
@@ -93,7 +64,7 @@ local M = {
     -- @options: classic, arrow, slant
     statusline = "classic",
     -- @options: gruvbox, neon, nord, wombat
-    statusline_color = "nord",
+    statusline_color = "neon",
     -----------------------------------------------------------------------------//
     --- LSP {{{1
     -----------------------------------------------------------------------------//
@@ -105,14 +76,10 @@ local M = {
     -- @value: boolean
     lsp_virtual_text = false,
 
-    -- Borders for the floating windows of LSP (e. diagnostics).
+    -- Borders for the floating windows of LSP (e.g. diagnostics).
     -- @value: string
     -- @options: single, double, rounded, none
-    lsp_window_borders = "single",
-
-    -- Show function signature when you type.
-    -- @value: boolean
-    lsp_signature_help = true,
+    lsp_window_borders = "rounded",
 
     --  Enable or disable signs from LSP in signcolum.
     -- @value: boolean
@@ -136,11 +103,6 @@ local M = {
     -- menu has to trigger manually with <C-Space> or <Tab>
     -- @value: boolean
     compe_autocomplete = true,
-
-    -- Borders for documentation in completion menu.
-    -- @value: string
-    -- @options: single, double, none
-    compe_doc_window_border = "single",
 
     -- Add description text to the suggested items in the completion menu.
     -- @value: boolean
